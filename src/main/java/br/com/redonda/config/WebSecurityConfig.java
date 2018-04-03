9ce3284324a0f999660b973a263bd9f1b05b693e
Eduardo Morgon <eduardo.morgon@gmail.com>
2018-04-03 10:51:07 -0300

    Configuração do cors.

--- src/main/java/br/com/redonda/config/WebSecurityConfig.java
+++ src/main/java/br/com/redonda/config/WebSecurityConfig.java
@@ -9,7 +9,9 @@ import br.com.redonda.security.JWTAuthenticationFilter;
 import br.com.redonda.security.JWTLoginFilter;
 import br.com.redonda.security.JwtAuthenticationEntryPoint;
 import br.com.redonda.security.TokenAuthenticationService;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,6 +20,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  *
@@ -38,6 +43,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
         http.csrf()
                 .disable()
+                
+                .cors()
+                .and()
+                
                 .exceptionHandling()
                 .authenticationEntryPoint(unauthorizedHandler)
                     
@@ -53,6 +62,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 
+    }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.addExposedHeader("Authorization");
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Override
